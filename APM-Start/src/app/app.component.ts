d51d8643c51727061ad60aa7ecd69af82ff23da0
Md Kamruzzaman <mkzamanalive@gmail.com>
2018-03-22 18:01:11 +0600

    component understanding

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+     <div class="container">
+        <h1>
+        {{pageTitle}}
+        </h1>
+        <div>
+        <pm-products></pm-products>
+        </div>
+     </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle: string = 'Acme Product Management'
 }
